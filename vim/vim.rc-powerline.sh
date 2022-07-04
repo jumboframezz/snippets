19516cd853f366a3ef52577697e71f2252bbf2ed
Lachezar Georgiev <lachezarg@nsogroup.com>
2022-07-04 10:57:00 +0300

    Added vim

--- vim/vim.rc-powerline.sh
+++ vim/vim.rc-powerline.sh
@@ -1,5 +1,10 @@
-cat >> ~/.vimrc << EOF
+# Set system-wide 
+cat>> /etc/vimrc << EOF
+" .yaml settings for vim"
+autocmd FileType yaml setlocal ai ts=2 sw=2 et
+autocmd FileType yml setlocal ai ts=2 sw=2 et
 
+" powerline config"
 python3 import sys
 python3 sys.path.insert(0, "~/.local/lib/python3.10/site-packages/")
 
@@ -7,6 +12,25 @@ python3 from powerline.vim import setup as powerline_setup
 python3 powerline_setup()
 python3 del powerline_setup
 
+" Enable numbers"
 set laststatus=2
-set number
+EOF
+
+# Set per user
+cat >> ~/.vimrc << EOF
+" .yaml settings for vim"
+autocmd FileType yaml setlocal ai ts=2 sw=2 et
+autocmd FileType yml setlocal ai ts=2 sw=2 et
+
+" powerline config"
+python3 import sys
+python3 sys.path.insert(0, "~/.local/lib/python3.10/site-packages/")
+
+python3 from powerline.vim import setup as powerline_setup
+python3 powerline_setup()
+python3 del powerline_setup
+
+" Enable numbers"
+set laststatus=2
+
 EOF
