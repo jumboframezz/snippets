db57c0176578ab982353158a367c3e0d500ae3f5
Lachezar G <lucho@decard>
2023-06-19 16:41:00 +0300

    EOD

---
+++ bash_substitution.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+## Set current_user to $1 if not specified current_user=$USER
+    current_user=${1:-$USER}
+    echo "Current user: $current_user"
+
+
+## Assign a value foo to the $USER variable if doesn’t already have one:
+    unset $var1
+    echo "${var1:=default_value}"
+
+
+## Display error if var1 is not defined:
+    unset var1
+    echo "${var1?Error var1 is not defined}"
+
+
+## Remove patterns in front:
+    var1="/etc/resolv.conf"
+    echo "${var1#/etc/}" 
+
+    var1=https://ds01.domain.internal/ipa/ui/#/e/user/search/page=3
+    echo "${var1#*/}"   # /ds01.domain.internal/ipa/ui/#/e/user/search/page=3
+    echo "${var1##*/}"  # page=3
+
+
+## Remove patterns in end:
+    var1="bash-1.3.0.tar.gz"
+    echo "${var1%.tar.gz}"
+
+## Replace patterns
+    var1="Use unix or die"
+    echo "${var1/unix/linux}"
+
+# nice way to make backup:
+    file=/etc/resolv.conf
+    cp "${file}" "${file/.conf/.conf.bak}"
+
+
+## Slicing as in python
+var1="0123456789"
+echo "${var1:4}"
+echo "${var1:4:2}"
+
+
+## Get list of matching variable names
+var1="Bus"
+var2="Car"
+var3="Train"
+echo "${!var*}"
+
+
+## Convert to upper to lower case or vice versa
+name="lachezar"
+echo "${name^}"
+echo "${name^^}"
+# Convert everything to lowercase
+    dest="/HOME/Lucho/DaTA"
+    echo "Actual path: ${dest,,}"
+# Convert only first character to lowercase 
+    src="HOME"
+    echo "${src,}"
+# Only convert first character in $dest if it is a capital ‘H’:
+    dest="Home"
+    echo "${dest,H}"
+    dest="Fhome"
+    echo "${dest,H}"
+
+
+# placeholder
--- tls_connect.sh
+++ tls_connect.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 declare -A colors=(['red']='\e[31m' ['green']='\e[32m' ['yellow']='\e[33m' ['blue']='\e[34m' ['magenta']='\e[35m' ['cyan']='\e[36m' \
                 [light_red]='\e[91m' ['light_green']='\e[92m' [light_yellow]='\e[93m' [light_blue]='\e[94m'\
                 [bold]='\e[1m' ['no_color']='\033[0m' [bold_green]='\e[1;\e[32m' [bold_white]='\e[1;\e[97m'  )
@@ -13,8 +14,5 @@ err_exit(){
     exit "${2:-0}" 
 }
 
-[ ! -d .git ] && err_exit "fatal: not a git repository : .git"
-for remote in $( git remote -v | grep -o  '^\w*' |uniq); do 
-    _echo "Pushing to remote $remote" green
-    git push "$remote"
-done
+tls_domain=$1
+openssl s_client -showcerts -servername "$tls_domain" -connect "$tls_domain":443 </dev/null
