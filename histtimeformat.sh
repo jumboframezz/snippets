d0babaee6af2540866d739acb647a1911eb62788
Lucho <lgeorgieff@gmail.com>
2023-06-25 17:32:30 +0300

    Coror hint in bash history

--- histtimeformat.sh
+++ histtimeformat.sh
@@ -4,6 +4,10 @@ dst=/etc/profile
 # timedatectl set-timezone UTC
 # export TZ="Europe/Sofia"
 # export HISTTIMEFORMAT="%d-%b-%y %T "
+# Color hint:
+# MY_BASH_BLUE="\033[0;34m" #Blue
+# MY_BASH_NOCOLOR="\033[0m"
+# HISTTIMEFORMAT=$(echo -e ${MY_BASH_BLUE}[%F %T] $MY_BASH_NOCOLOR)
 
 [ -f "$dst" ] && echo 'export HISTTIMEFORMAT="%d-%b-%y %T "' >> /etc/profile \
     || echo "$dst file not found"
