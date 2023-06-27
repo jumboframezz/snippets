757458acfa6ac3f4d6d982fc71c0b2d8b176fc42
Lucho <lgeorgieff@gmail.com>
2023-06-27 19:36:03 +0300

    generate .ovpn file

--- openvpn-new-client.sh
+++ openvpn-new-client.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # This generates new openvon client. Not tested 
 err_exit(){
-    echo "$1"
-    exit "$2"
+    echo "$1" && exit "$2"
 }
 
 client_name="t480-june"
@@ -12,3 +11,35 @@ mkdir -p $dst_dir
 cd /etc/easy-rsa/ || return
 ./easyrsa build-client-full $client_name nopass && \
     cp -rp /etc/easy-rsa/pki/{ca.crt,issued/$client_name.crt,private/$client_name.key} $dst_dir
+
+server_ip=$(dig lz1lgg.eu -4 +short)
+
+cat <<EOF
+client
+tls-client
+pull
+dev tun
+proto udp4
+remote $server_ip  1194
+
+data-ciphers AES-256-GCM 
+# data-ciphers-fallback BF-CBC
+auth SHA256
+resolv-retry infinite
+nobind
+user nobody
+group nobody
+persist-key
+persist-tun
+key-direction 1
+remote-cert-tls server
+auth-nocache
+# comp-lzo
+verb 3
+auth SHA512
+tls-auth ta.key 1
+ca /home/lucho/openvn-client/ca.crt
+cert /home/lucho/openvn-client/client.crt
+key /home/lucho/openvn-client/client.key
+
+EOF
